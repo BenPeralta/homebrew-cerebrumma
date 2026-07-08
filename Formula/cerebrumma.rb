99b2493bcad49d9a02403e94573f25b9fb815601
BenPeralta <benperalta24@gmail.com>
2026-07-08 16:25:03 -0500

    cerebrumma 0.1.4 — global model cache fix

--- Formula/cerebrumma.rb
+++ Formula/cerebrumma.rb
@@ -5,19 +5,19 @@ class Cerebrumma < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/BenPeralta/cerebrumma/releases/download/v0.1.3/cerebrum-aarch64-apple-darwin.tar.gz"
-      sha256 "909062734c230b663c739b1a97224a349fb7ca90bd11ee82f3f6f70d0305edb5"
+      url "https://github.com/BenPeralta/cerebrumma/releases/download/v0.1.4/cerebrum-aarch64-apple-darwin.tar.gz"
+      sha256 "4357ba8c9200b252e873bbf63229a105cf74d37fa7751ee1f28e2343195113dc"
     end
     on_intel do
-      url "https://github.com/BenPeralta/cerebrumma/releases/download/v0.1.3/cerebrum-x86_64-apple-darwin.tar.gz"
-      sha256 "c8d504b74acaf24750a01e069b4b704b87c36b5d1a16547e8ee2aa37a57eb8ea"
+      url "https://github.com/BenPeralta/cerebrumma/releases/download/v0.1.4/cerebrum-x86_64-apple-darwin.tar.gz"
+      sha256 "a7532d0a0d10f1ead504953f96159a8a3de9d0a961fe78bc9966fa2f08a13c13"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/BenPeralta/cerebrumma/releases/download/v0.1.3/cerebrum-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "520c4bb7810a28c65afec2f1db653cce6e8cf4b2292ada5e73ec90a9fc673783"
+      url "https://github.com/BenPeralta/cerebrumma/releases/download/v0.1.4/cerebrum-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "647dfd304a6063c063d746361f9db22ed116f75f3b3d465564e54ab48084cdd2"
     end
   end
 
